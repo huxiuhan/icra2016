134322410c77b6085c2674052ffae3414bedea46
Xiuhan Hu <huxiuhan@gmail.com>
2016-02-22 21:58:36 +0800

    cookie selector

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,11 +1,19 @@
-$('#stages-carousel').carousel({
+var $carousel = $('#stages-carousel');
+$carousel.carousel({interval: false});
+var stgcls = Cookies.get('stage');
+if (stgcls != undefined) {
+  $('.stage').removeClass('active');
+  $(stgcls).addClass('active');
+  $carousel.carousel(Number($(stgcls).attr('data-slide-to')));
+}
+
+$carousel.carousel({
   interval: false,
 });
 
-
-$('#stages-carousel').on('slide.bs.carousel', function (event) {
+$carousel.on('slide.bs.carousel', function (event) {
   var stgcls = '.stage-'+event.relatedTarget.id.split('-')[0];
   $('.stage').removeClass('active');
   $(stgcls).addClass('active');
-})
-
+  Cookies.set('stage', stgcls);
+});
